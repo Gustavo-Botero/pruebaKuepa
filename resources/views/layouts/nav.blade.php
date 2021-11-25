af2e41613ee366f11b9314f4d4dc7fceff680c0d
Gustavo Botero <gustavoadolfob92@gmail.com>
2021-11-24 19:21:52 -0500

    Se ajusto la plantilla para comenzar a trabajar con los formularios

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,23 @@
+<nav class="mt-2">
+    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+      <!-- Add icons to the links using the .nav-icon class
+           with font-awesome or any other icon font library -->
+      <li class="nav-item">
+        <a href="#" class="nav-link">
+          <i class="nav-icon fas fa-tachometer-alt"></i>
+          <p>
+            Registro
+            <i class="right fas fa-angle-left"></i>
+          </p>
+        </a>
+      </li>
+      <li class="nav-item">
+        <a href="../widgets.html" class="nav-link">
+          <i class="nav-icon fas fa-th"></i>
+          <p>
+            Estudiantes
+          </p>
+        </a>
+      </li>
+    </ul>
+  </nav>
